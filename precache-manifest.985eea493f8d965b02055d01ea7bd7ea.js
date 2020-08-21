f06c1b9c835a71b433eeade4b9bca2402d077984
Cody Henderson <codyscotthenderson@gmail.com>
2020-08-20 21:48:44 -0400

    Updates

---
+++ precache-manifest.985eea493f8d965b02055d01ea7bd7ea.js
@@ -0,0 +1,314 @@
+self.__precacheManifest = [
+  {
+    "revision": "66fd20b3855e915f9e58",
+    "url": "/test/static/js/app.20e2debf.chunk.js"
+  },
+  {
+    "revision": "a1e2cb67a610854e158e",
+    "url": "/test/static/js/runtime~app.39ceea80.js"
+  },
+  {
+    "revision": "b4823bb87b6993a6ca55",
+    "url": "/test/static/js/2.5812b55a.chunk.js"
+  },
+  {
+    "revision": "75cd4693edc1d35ea56b9a65d7926a18",
+    "url": "/test/static/media/exclude.46c8feb2.png"
+  },
+  {
+    "revision": "cac0696d39f945c2424a76bef01dcb64",
+    "url": "/test/static/media/class_small_demonhunter.27bc28b0.png"
+  },
+  {
+    "revision": "8e4f71025da9f9e3698d4718dbb73cc0",
+    "url": "/test/static/media/class_small_druid.b2cf4d40.png"
+  },
+  {
+    "revision": "b8822d505a354c3d67b3297baae978f6",
+    "url": "/test/static/media/class_small_hunter.982db222.png"
+  },
+  {
+    "revision": "5f0d17ad2a983442fce3b354e7a60914",
+    "url": "/test/static/media/class_small_mage.9b3b210e.png"
+  },
+  {
+    "revision": "c235dd4ddf5ccdc5a9468323cf28fb99",
+    "url": "/test/static/media/class_small_paladin.ca95d589.png"
+  },
+  {
+    "revision": "5b829ab341ef6d69eac39284e4b94089",
+    "url": "/test/static/media/class_small_priest.74da5ea8.png"
+  },
+  {
+    "revision": "5aa901e0a0b86659878427ba6556269d",
+    "url": "/test/static/media/class_small_rogue.a1f42042.png"
+  },
+  {
+    "revision": "cf979434fbd805999a9ecfd783996963",
+    "url": "/test/static/media/class_small_shaman.6ebd9c99.png"
+  },
+  {
+    "revision": "54434d8b007519417ecc4b2fee5705c2",
+    "url": "/test/static/media/class_small_warlock.02204f8b.png"
+  },
+  {
+    "revision": "4405926d24c16e4f401b907969e0ea64",
+    "url": "/test/static/media/class_small_warrior.8656e5cc.png"
+  },
+  {
+    "revision": "e2fdb0dec4f7c3e792bf387a1f226fb2",
+    "url": "/test/static/media/class_small_neutral.d25d7174.png"
+  },
+  {
+    "revision": "be775a6ef238702405d0fed963e43044",
+    "url": "/test/static/media/Set1_Icon.ac32ff2e.png"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/test/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/test/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/test/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/test/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/test/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/test/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/test/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/test/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "4e15a2939a15dc5567da51d68116417c",
+    "url": "/test/static/media/kofi.96681ac5.png"
+  },
+  {
+    "revision": "8f8d12b8691a706a99e7544bd33527c2",
+    "url": "/test/static/media/bitcoin.ab09bc27.png"
+  },
+  {
+    "revision": "eeff16cc73d9458c9a865ddb1542c34d",
+    "url": "/test/static/media/litecoin.e0409b43.png"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/test/./fonts/Roboto_100Thin.ttf"
+  },
+  {
+    "revision": "35a9c89aff1396595ad345e378a32aca",
+    "url": "/test/./fonts/Roboto_100Thin_Italic.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/test/./fonts/Roboto_300Light.ttf"
+  },
+  {
+    "revision": "a3ce4440f2abf76f4a1b14b83920138c",
+    "url": "/test/./fonts/Roboto_300Light_Italic.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/test/./fonts/Roboto_400Regular.ttf"
+  },
+  {
+    "revision": "a720f17aa773e493a7ebf8b08459e66c",
+    "url": "/test/./fonts/Roboto_400Regular_Italic.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/test/./fonts/Roboto_500Medium.ttf"
+  },
+  {
+    "revision": "cf23e1bb619029496260760b72aebd30",
+    "url": "/test/./fonts/Roboto_500Medium_Italic.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/test/./fonts/Roboto_700Bold.ttf"
+  },
+  {
+    "revision": "5b44818d2b9eda3e23cd5edd7b49b7d5",
+    "url": "/test/./fonts/Roboto_700Bold_Italic.ttf"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/test/./fonts/Roboto_900Black.ttf"
+  },
+  {
+    "revision": "99682a78fa4fe61e1177b94757336bbf",
+    "url": "/test/./fonts/Roboto_900Black_Italic.ttf"
+  },
+  {
+    "revision": "65b22f0323723ac333a398b6c74f71e5",
+    "url": "/test/static/media/common_gem_small.c3cb9701.png"
+  },
+  {
+    "revision": "c6c5e26fb0e4601e6ea27d19bc176e56",
+    "url": "/test/static/media/rare_gem_small.f2054728.png"
+  },
+  {
+    "revision": "b07f901f15229a0650405f6d96fc7c0d",
+    "url": "/test/static/media/epic_gem_small.2056dd5b.png"
+  },
+  {
+    "revision": "eaaea84078817b865a400a68a29c7c87",
+    "url": "/test/static/media/legendary_gem_small.60d2f3cd.png"
+  },
+  {
+    "revision": "1d69b9589be26c85d295fd8d3a98da58",
+    "url": "/test/static/media/HallOfFameIcon.37379c27.png"
+  },
+  {
+    "revision": "1f9249f11808e9f6b69c89777c8e9342",
+    "url": "/test/static/media/NaxxIcon.3b105f8e.png"
+  },
+  {
+    "revision": "fbdc7af165ce8fbad0afa6461aa4c770",
+    "url": "/test/static/media/GvGIcon.8743247a.png"
+  },
+  {
+    "revision": "684f0eab54e5164d1c450869f943bbbd",
+    "url": "/test/static/media/BRMIcon.446540f2.png"
+  },
+  {
+    "revision": "915d5638ad755124cf72ce8f0bfa34e0",
+    "url": "/test/static/media/TGTIcon.253b1f48.png"
+  },
+  {
+    "revision": "2317a2a1f005f66a038eada5cf6589c1",
+    "url": "/test/static/media/LOEIcon.9975dfb8.png"
+  },
+  {
+    "revision": "e50df9cfb74fff1317e3ba27549f3ecc",
+    "url": "/test/static/media/OGIcon.53624503.png"
+  },
+  {
+    "revision": "1e4e6c526b49ae60b6ada2db7da20fd1",
+    "url": "/test/static/media/KaraIcon.c214d778.png"
+  },
+  {
+    "revision": "1e85411cb40c5e05a6c965046f3b1686",
+    "url": "/test/static/media/GangsIcon.be4ad498.png"
+  },
+  {
+    "revision": "c039773b0f574ead7b67430f38e6a840",
+    "url": "/test/static/media/UNGIcon.01f6a5f3.png"
+  },
+  {
+    "revision": "192f90e4f1dca62b69ec8e6a0fbaf1a3",
+    "url": "/test/static/media/ICCIcon.45a747ea.png"
+  },
+  {
+    "revision": "486dc7f657f2eb94fd7cca2329ffd985",
+    "url": "/test/static/media/LOOTIcon.edfb0176.png"
+  },
+  {
+    "revision": "aaf4a10e60755b6039d9bc61a86b3889",
+    "url": "/test/static/media/GILIcon.836508be.png"
+  },
+  {
+    "revision": "6297efee37f614e049c591fe07f3fc78",
+    "url": "/test/static/media/BOTIcon.e5ff7520.png"
+  },
+  {
+    "revision": "552d238868273035675c39bd35d64740",
+    "url": "/test/static/media/TRLIcon.a700a78e.png"
+  },
+  {
+    "revision": "02c72e250e4556623cc84a8bd050fe68",
+    "url": "/test/static/media/DALIcon.88d1b880.png"
+  },
+  {
+    "revision": "26fc0ab4c12964cfdb5e8966309c97af",
+    "url": "/test/static/media/UldumIcon.76a3f13f.png"
+  },
+  {
+    "revision": "d54d92be7130e94b054215fa5b765e7c",
+    "url": "/test/static/media/DRGIcon.bde25580.png"
+  },
+  {
+    "revision": "5c41479d4be4651bf6ca69072057332b",
+    "url": "/test/static/media/DRGIcon_MiniSet.a5fcd6d5.png"
+  },
+  {
+    "revision": "a6827916653fb552ee9ef390d338ff5c",
+    "url": "/test/static/media/DHIIcon.3c827183.png"
+  },
+  {
+    "revision": "831ab0ce560a8bfecb3dab4ab3538db5",
+    "url": "/test/static/media/BTIcon.1c69bd42.png"
+  },
+  {
+    "revision": "d171b67a96c2abf4685f5f764cc5bfaf",
+    "url": "/test/static/media/SCHIcon.5e6aa75c.png"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/test/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/test/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/test/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/test/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "f328c398676e066fb4e76f0c028592b4",
+    "url": "/test/expo-service-worker.js"
+  },
+  {
+    "revision": "7a08c64710dd75bc390a365394122b1d",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "e6bfb1796dcb9e67c7d90c123c1ae2f7",
+    "url": "/test/register-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/test/serve.json"
+  },
+  {
+    "revision": "3c944d27a8e8bea454129d0bca04be04",
+    "url": "/test/static/js/2.5812b55a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e932a83d1b38bc43a980931beecedcea",
+    "url": "/test/favicon-16.png"
+  },
+  {
+    "revision": "d2250ae0203f2ef21397398343ab19e5",
+    "url": "/test/favicon-32.png"
+  },
+  {
+    "revision": "5dc008b084d255270d7b522a826319d2",
+    "url": "/test/favicon.ico"
+  },
+  {
+    "revision": "5a85037059731d7c27ae5a9ee8e412d6",
+    "url": "/test/manifest.json"
+  }
+];
